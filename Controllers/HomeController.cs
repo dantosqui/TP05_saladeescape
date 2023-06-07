8d52faa6e40009d74b87e64a05e1879482491819
ZAREK ALEXIS NAZAR DUTKA <47696115@ort>
2023-06-07 09:01:06 -0300

    css stileado un toque y carta arreglado

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -34,6 +34,10 @@ public class HomeController : Controller
     {
         return View();
     }
+    public IActionResult VolverCarta(){
+        ViewBag.incorrecto="";
+        return View("habitacion0");
+    }
     public IActionResult Creditos()
     {
         return View();
